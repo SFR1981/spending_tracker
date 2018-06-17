0603f1b07ceaf46ebd075e268ece576af1de070b
Stephen Rooney <s.f.rooney@gmail.com>
2018-06-17 15:42:53 +0100

    tags controller begin

--- controllers/tag_controller.rb
+++ controllers/tag_controller.rb
@@ -1,5 +1,18 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all')
 require( 'pry-byebug')
-require_relative( './models/tag.rb')
+require_relative( '../models/tag.rb')
 also_reload('./models*')
+
+
+
+
+get '/tags' do
+  @tags = Tag.all()
+  erb( :"tag/index" )
+end
+
+get '/tags/:id' do
+  @tag = Tag.find(params['id'].to_i)
+  erb( :"tag/show" )
+end
